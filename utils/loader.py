3174ad49212c2a52352e763d5b0ae0e852dc8609
Tashyia23 <juhitashyiakaur@gmail.com>
2025-11-24 03:37:57 +0800

    Update loader.py

--- utils/loader.py
+++ utils/loader.py
@@ -1,25 +1,10 @@
+import pandas as pd
 import streamlit as st
 
-@st.cache_data(show_spinner=False)
-def load_master():
-    """Returns the master dataset dict."""
-    return load_master_dataset()
+@st.cache_data
+def load_raw_global():
+    return pd.read_csv("data/raw/global_air_pollution.csv")
 
-
-def load_time_series():
-    """Loads PM2.5 time series data."""
-    master = load_master()
-    return master.get("time_series")
-
-
-def load_pollutant_index():
-    """Loads pollutant breakdown dataset."""
-    master = load_master()
-    return master.get("pollutant_index")
-
-
-def load_risk_index():
-    """Loads health & risk index dataset."""
-    master = load_master()
-    return master.get("risk_index")
-
+@st.cache_data
+def load_raw_pm25():
+    return pd.read_csv("data/raw/pm25_air_pollution.csv")
