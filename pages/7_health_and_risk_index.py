9b80d02210e81a2b80a8b1bf2681dbc918b60051
Tashyia23 <juhitashyiakaur@gmail.com>
2025-11-24 06:19:33 +0800

    Update 7_health_and_risk_index.py

--- pages/7_health_and_risk_index.py
+++ pages/7_health_and_risk_index.py
@@ -273,14 +273,14 @@ if view_mode.startswith("Before"):
         st.warning("⚠ Please choose two *different* pollutants for comparison.")
     else:
         fig_scatter = px.scatter(
-            raw_g,
-            x=sc_x,
-            y=sc_y,
-            title=f"{pretty_raw_labels[sc_x]} vs {pretty_raw_labels[sc_y]} (Raw AQI)",
-            trendline="ols",
-            opacity=0.7,
-            color_discrete_sequence=["#0EA5E9"]
-        )
+           raw_g,
+           x=sc_x,
+           y=sc_y,
+           title=f"{pretty_raw_labels[sc_x]} vs {pretty_raw_labels[sc_y]} (Raw AQI)",
+           opacity=0.7,
+           color_discrete_sequence=["#0EA5E9"]
+         )
+
         fig_scatter.update_layout(height=450)
         st.plotly_chart(fig_scatter, use_container_width=True)
 
